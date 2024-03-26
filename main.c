c56cc77b9f8bd2d23c50a30e09f2d65c5352f4fc
Joshua N <89023959+joshuan98@users.noreply.github.com>
2024-03-26 01:57:24 -0400

    T motor control (#14)
    * Add message queue for tMotorControl
    
    * Remove ignored artefacts
    
    * Update code based on comments

--- main.c
+++ main.c
@@ -10,42 +10,100 @@
 
 static void delay(uint32_t milliseconds)
 {
-	volatile uint32_t nof = milliseconds * 4800;
-	while (nof != 0)
-	{
-		__asm("NOP");
-		nof--;
-	}
+  volatile uint32_t nof = milliseconds * 4800;
+  while (nof != 0)
+  {
+    __asm("NOP");
+    nof--;
+  }
 }
 
-// 0 = Stationary, 1 = Moving
-volatile int isMoving = 0;
+osThreadId_t tid_movingLEDThread;
+osThreadId_t tid_stationaryLEDThread;
 
-// Only accessed by perform movement thread for now
-// At global to be seen in debugger
-struct UartValues uartValues;
-struct MotorSpeed motorSpeed;
-volatile uint8_t button;
+int turnOff = 0;
+int turnOn = 1;
+void movingLEDThread(void *argument)
+{
+  int greenCounter = 0;
+  int isRedOn = 1;
+  for (;;)
+  {
+    osThreadFlagsWait(0x0001, osFlagsWaitAny, osWaitForever);
+    // Moving
+    for (int i = 0; i < NUM_LEDS; i++)
+    // Turn on 1 green LED at a time
+    {
+      ledControl(GREEN, turnOff, i);
+    }
+    ledControl(GREEN, turnOn, greenCounter);
+    greenCounter = greenCounter >= NUM_LEDS - 1 ? 0 : greenCounter + 1;
+    // Toggle all red LEDs
+    ledControl(RED, isRedOn, 0);
+    isRedOn = isRedOn == 1 ? 0 : 1;
+    osDelay(500);
+  }
+}
+
+void stationaryLEDThread(void *argument)
+{
+  int isRedOn = 1;
+  for (;;)
+  {
+    osThreadFlagsWait(0x0001, osFlagsWaitAny, osWaitForever);
+    // Stationary
+    // Turn on all green
+    for (int i = 0; i < NUM_LEDS; i++)
+    {
+      ledControl(GREEN, turnOn, i);
+    }
+    // Toggle all red LEDs
+    ledControl(RED, isRedOn, 0);
+    isRedOn = isRedOn == 1 ? 0 : 1;
+    osDelay(250);
+  }
+}
+
+osMessageQueueId_t uartValuesMessageQueue;
 
 void tBrain(void *argument)
 {
-  // Should instead send some value to motor thread
-  // which handles the movement
+  UartValues_t myUartData;
+
+  tid_movingLEDThread = osThreadNew(movingLEDThread, NULL, NULL);
+  tid_stationaryLEDThread = osThreadNew(stationaryLEDThread, NULL, NULL);
+
   while (1)
   {
-    uartValues = extractUartValues();
-		button = uartValues.button;
-		if (button == 1)
-		{
-			setCourseEnded(true);
-		}
-    motorSpeed = calculateSpeed(uartValues.x_axis, uartValues.y_axis);
-    moveAll(motorSpeed.leftSpeed, motorSpeed.rightSpeed);
+    myUartData = extractUartValues();
+
+    osMessageQueuePut(uartValuesMessageQueue, &myUartData, NULL, 0);
+
+    if (myUartData.button == 1)
+    {
+      setCourseEnded(true);
+    }
+
+    if (myUartData.x_axis != 4 || myUartData.y_axis != 4)
+    {
+      osThreadFlagsSet(tid_movingLEDThread, 0x0001);
+    }
+    else
+    {
+      osThreadFlagsSet(tid_stationaryLEDThread, 0x0001);
+    }
   }
 }
 
 void tMotorControl(void *argument)
 {
+  UartValues_t myUartData;
+  for (;;)
+  {
+    osMessageQueueGet(uartValuesMessageQueue, &myUartData, NULL, osWaitForever);
+    struct MotorSpeed motorSpeed = calculateSpeed(myUartData.x_axis, myUartData.y_axis);
+    moveAll(motorSpeed.leftSpeed, motorSpeed.rightSpeed);
+  }
 }
 
 /**
@@ -63,88 +121,30 @@ void tMotorControl(void *argument)
  * Clock for timing the flashing
  **/
 
-osThreadId_t tid_movingLEDThread;
-osThreadId_t tid_stationaryLEDThread;
-
-int turnOff = 0;
-int turnOn = 1;
-void movingLEDThread(void *argument)
-{
-	int greenCounter = 0;
-	int isRedOn = 1;
-	for (;;)
-	{
-		osThreadFlagsWait(0x0001, osFlagsWaitAny, osWaitForever);
-		// Moving
-		for (int i = 0; i < NUM_LEDS; i++)
-		// Turn on 1 green LED at a time
-		{
-			ledControl(GREEN, turnOff, i);
-		}
-		ledControl(GREEN, turnOn, greenCounter);
-		greenCounter = greenCounter >= NUM_LEDS - 1 ? 0 : greenCounter + 1;
-		// Toggle all red LEDs
-		ledControl(RED, isRedOn, 0);
-		isRedOn = isRedOn == 1 ? 0 : 1;
-		osDelay(500);
-	}
-}
-
-void stationaryLEDThread(void *argument)
-{
-	int isRedOn = 1;
-	for (;;)
-	{
-		osThreadFlagsWait(0x0001, osFlagsWaitAny, osWaitForever);
-		// Stationary
-		// Turn on all green
-		for (int i = 0; i < NUM_LEDS; i++)
-		{
-			ledControl(GREEN, turnOn, i);
-		}
-		// Toggle all red LEDs
-		ledControl(RED, isRedOn, 0);
-		isRedOn = isRedOn == 1 ? 0 : 1;
-		osDelay(250);
-	}
-}
-
 void tLED(void *argument)
 {
-	tid_movingLEDThread = osThreadNew(movingLEDThread, NULL, NULL);
-	tid_stationaryLEDThread = osThreadNew(stationaryLEDThread, NULL, NULL);
-	for (;;)
-	{
-		if (motorSpeed.leftSpeed != 0 || motorSpeed.rightSpeed != 0)
-		{
-			osThreadFlagsSet(tid_movingLEDThread, 0x0001);
-		}
-		else
-		{
-			osThreadFlagsSet(tid_stationaryLEDThread, 0x0001);
-		}
-	}
 }
 
-
-void tAudio(void* argument){
-	playBeginningTheme();
+void tAudio(void *argument)
+{
+  playBeginningTheme();
 }
 
 int main(void)
 {
-	// Initialization
-	SystemCoreClockUpdate();
-	initPWM();
-	initLEDs();
-	initAudioPWM();
-	initUART2(BAUD_RATE);
-	osKernelInitialize(); // Initialize CMSIS-RTOS
+  // Initialization
+  SystemCoreClockUpdate();
+  initPWM();
+  initLEDs();
+  initAudioPWM();
+  initUART2(BAUD_RATE);
+  osKernelInitialize(); // Initialize CMSIS-RTOS
 
-	// Threads
-	osThreadNew(tBrain, NULL, NULL);
-	osThreadNew(tMotorControl, NULL, NULL);
-	osThreadNew(tLED, NULL, NULL);
-	osThreadNew(tAudio, NULL, NULL);
-	osKernelStart();
+  // Threads
+  uartValuesMessageQueue = osMessageQueueNew(1, sizeof(UartValues_t), NULL);
+  osThreadNew(tBrain, NULL, NULL);
+  osThreadNew(tMotorControl, NULL, NULL);
+  // osThreadNew(tLED, NULL, NULL);
+  osThreadNew(tAudio, NULL, NULL);
+  osKernelStart();
 }
