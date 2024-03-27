a53df6810cb1ae51fc2e0e0a038947b5bd57bddf
Toh-HongFeng <hf.toh2@gmail.com>
2024-03-27 15:38:01 +0800

    refactor: cleans up unused functions

--- motor.c
+++ motor.c
@@ -69,38 +69,6 @@ void stopMotor(void)
 	TPM2_C1V = 0; // RIGHT_BACK_W
 }
 
-void move(uint16_t speed)
-{
-	TPM1_C0V = speed; // LEFT_FRONT_W
-	// TPM1_C1V = 0; 		// LEFT_BACK_W
-	TPM2_C0V = speed; // RIGHT_FRONT_W
-					  // TPM2_C1V = 0; 		// RIGHT_BACK_W
-}
-
-void reverse(uint16_t speed)
-{
-	// TPM1_C0V = 0;// LEFT_FRONT_W
-	TPM1_C1V = speed; // LEFT_BACK_W
-	// TPM2_C0V = 0;  // RIGHT_FRONT_W
-	TPM2_C1V = speed; // RIGHT_BACK_W
-}
-
-void goLeft(uint16_t speed)
-{
-	// TPM1_C0V = 0; 	// LEFT_FRONT_W
-	// TPM1_C1V = 0; 		// LEFT_BACK_W
-	TPM2_C0V = speed; // RIGHT_FRONT_W
-					  // TPM2_C1V = 0; 		// RIGHT_BACK_W
-}
-
-void goRight(uint16_t speed)
-{
-	TPM1_C0V = speed; // LEFT_FRONT_W
-					  // TPM1_C1V = 0; 		// LEFT_BACK_W
-					  // TPM2_C0V = 0;  // RIGHT_FRONT_W
-					  // TPM2_C1V = 0; 		// RIGHT_BACK_W
-}
-
 void moveAll(int16_t leftSpeed, int16_t rightSpeed)
 {
 	if (leftSpeed == 0)
